d058e86320bfead235bab7112ad1ca61773fd05e
Vikkans <Jakobsenviktor6@gmail.com>
2023-09-15 12:37:50 +0200

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            string x = Console.ReadLine();
+            double y = double.Parse(x);
+
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            string z = Console.ReadLine();
+            double g = double.Parse(z);
+
+            double sum = y - g;
+
+            Console.WriteLine("Elin hoppade " + sum + " meter längre än Alma");
+            Console.ReadKey();
+
+        }
+        
+
+
+
+
+    }
+
+
+
+}
